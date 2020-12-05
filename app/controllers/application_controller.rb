961f80b6fd7c97180d34df79ac4558b03c423c81
michael <mpthornton86@gmail.com>
2020-12-04 17:54:20 -0700

    commit tree 03147b1675edde9844271c788bb9d8e5d8e51ab3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,16 +10,40 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  post '/login' do
+  get '/error' do
+    erb :error
+  end
 
+  post '/login' do
+    #binding.pry
+      @user = User.all.find_by_username(params[:username])
+      
+      if @user
+      session[:user_id] = @user.id
+     
+      redirect '/account'
+    else erb :error
+    end
   end
 
   get '/account' do
-
+    
+    if Helpers.is_logged_in?(session)
+      @user = User.all.find(session[:user_id])
+      #binding.pry
+    erb :account
+    else erb :error
+    end
   end
 
   get '/logout' do
-
+    session.destroy
+    redirect '/'
+  end
+  
+  post '/logout' do
+    
+    redirect '/'
   end
 
 
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,13 @@
-class Helpers
+class Helpers < ApplicationController
+    def self.current_user(args)
+        user = User.all.find(args[:user_id])
+        user
+    end
+
+    def self.is_logged_in?(args)
+        if args[:user_id]
+            true
+        else false
+        end
+    end
 end
